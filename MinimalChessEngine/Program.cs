7b9b220ca968df0669e80cd3053d4fc7ffc58dfc
lithander <lithander@gmx.de>
2020-11-30 12:35:35 +0100

    The Hello World of a chess engine: Added a text file ("engine-interface.txt") that specifies the UCI protocol. Implemented the absolute basics needed to register with Cute Chess and play a first pawn.

---
+++ MinimalChessEngine/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+
+namespace MinimalChessEngine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool running = true;
+            while (running)
+            {
+                string input = Console.ReadLine();
+                string[] tokens = input.Split();
+                switch (tokens[0])
+                {
+                    case "uci":
+                        Console.WriteLine("id name MinimalChessEngine");
+                        Console.WriteLine("uciok");
+                        break;
+                    case "isready":
+                        Console.WriteLine("readyok");
+                        break;
+                    case "position":
+                        break;
+                    case "go":
+                        Console.WriteLine("bestmove e2e4");
+                        break;
+                    case "ucinewgame":
+                        break;
+                    case "stop":
+                        break;
+                    case "quit":
+                        running = false;
+                        break;
+                    default:
+                        Console.WriteLine("UNKNOWN INPUT " + input);
+                        running = false;
+                        break;
+                }
+            }
+        }
+    }
+}
