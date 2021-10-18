c5c673447c0d7b79b50419cba327e8cd4c967459
lithander <lithander@gmx.de>
2021-10-18 02:50:14 +0200

    added annotated version of bishop move generation. also added a Bitboard.cs class with fast implementation of both rook & bishop move generation

---
+++ BitboardExplorer/Bitboard.cs
@@ -0,0 +1,121 @@
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.Intrinsics.X86;
+
+namespace BitboardExplorer
+{
+    static class Bitboard
+    {
+        const ulong DIAGONAL = 0x8040201008040201UL;
+        const ulong ANTIDIAGONAL = 0x0102040810204080UL;
+
+        const ulong HORIZONTAL = 0x00000000000000FFUL;
+        const ulong VERTICAL = 0x0101010101010101UL;
+
+        private static readonly ulong[] DIAGONALS = {
+            0x1, 0x102, 0x10204, 0x1020408, 0x102040810, 0x10204081020, 0x1020408102040,
+            0x102040810204080, 0x204081020408000, 0x408102040800000, 0x810204080000000,
+            0x1020408000000000, 0x2040800000000000, 0x4080000000000000, 0x8000000000000000
+        };
+
+        private static readonly ulong[] ANTIDIAGONALS = {
+            0x80, 0x8040, 0x804020, 0x80402010, 0x8040201008, 0x804020100804, 0x80402010080402,
+            0x8040201008040201, 0x4020100804020100, 0x2010080402010000, 0x1008040201000000,
+            0x804020100000000, 0x402010000000000, 0x201000000000000, 0x100000000000000
+        };
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static ulong GenBishop____(in ulong occupation, in int square)
+        {
+            ulong bbPiece = 1UL << square;
+            ulong bbBlocker = occupation & ~bbPiece;
+            //mask the bits below bbPiece
+            ulong bbBelow = bbPiece - 1;
+
+            int rank = square >> 3;
+            int file = square & 7;
+            //diagonal full line
+            ulong bbDiagonal = DIAGONALS[file + rank];
+            //antidiagonal full line 
+            ulong bbAntiDiagonal = ANTIDIAGONALS[rank + 7 - file];
+
+            return GenLines(bbDiagonal, bbAntiDiagonal, bbBlocker, bbBelow);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static ulong GenBishop__(in ulong occupation, in int square)
+        {
+            ulong bbPiece = 1UL << square;
+            ulong bbBlocker = occupation & ~bbPiece;
+            //mask the bits below bbPiece
+            ulong bbBelow = bbPiece - 1;
+
+            int diag = 8 * (square & 7) - (square & 56);
+            ulong bbDiagonal = (DIAGONAL >> (diag & (-diag >> 31))) << (-diag & (diag >> 31));
+
+            diag = 56 - 8 * (square & 7) - (square & 56);
+            ulong bbAntiDiagonal = (ANTIDIAGONAL >> (diag & (-diag >> 31))) << (-diag & (diag >> 31));
+
+            return GenLines(bbDiagonal, bbAntiDiagonal, bbBlocker, bbBelow);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static ulong GenBishop(in ulong occupation, in int square)
+        {
+            ulong bbPiece = 1UL << square;
+            ulong bbBlocker = occupation & ~bbPiece;
+            //mask the bits below bbPiece
+            ulong bbBelow = bbPiece - 1;
+
+            int rank = square >> 3;
+            int file = square & 7;
+            //diagonal full line
+            ulong bbDiagonal = VerticalShift(DIAGONAL, file - rank);
+            //antidiagonal full line 
+            ulong bbAntiDiagonal = VerticalShift(ANTIDIAGONAL, 7 - file - rank);
+
+            return GenLines(bbDiagonal, bbAntiDiagonal, bbBlocker, bbBelow);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static ulong GenRook(in ulong occupation, in int square)
+        {
+            ulong bbPiece = 1UL << square;
+            ulong bbBlocker = occupation & ~bbPiece;
+            //mask the bits below bbPiece
+            ulong bbBelow = bbPiece - 1;
+            //horizontal full line
+            ulong bbHorizontal = HORIZONTAL << (square & 56);
+            //vertical full line
+            ulong bbVertical = VERTICAL << (square & 7);
+
+            return GenLines(bbHorizontal, bbVertical, bbBlocker, bbBelow);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static ulong GenLines(in ulong bbLineA, in ulong bbLineB, in ulong bbBlocker, in ulong bbBelow) =>
+            GenLine(bbLineA, bbBlocker & bbLineA, bbBelow) |
+            GenLine(bbLineB, bbBlocker & bbLineB, bbBelow);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static ulong GenLine(in ulong bbLine, in ulong bbBlocker, in ulong bbBelow)
+        {
+            //MaskLow sets all low bits up to and including the lowest blocker above orgin, the rest are zeroed out.
+            //MaskHigh sets all low bits up to and including the highest blocker below origin, the rest are zerored out.
+            //The bits of the line that are different between the two masks are the valid targets (including the first blockers on each side)
+            return (MaskLow(bbBlocker & ~bbBelow) ^ MaskHigh(bbBlocker & bbBelow)) & bbLine;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        //identify the highest set bit and shift a mask so the bits below are set and the rest are zeroed
+        private static ulong MaskHigh(in ulong bb) => 0x7FFFFFFFFFFFFFFFUL >> (int)Lzcnt.X64.LeadingZeroCount(bb | 1);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        //identify the lowest set bit and set all bits below while zeroing the rest
+        private static ulong MaskLow(in ulong bb) => bb ^ (bb - 1);
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        //use sign of 'ranks' to decide between a left shift or right shift, then convert signed ranks to a positiver number of bits. each rank has 8 bits e.g. 1 << 3
+        private static ulong VerticalShift(in ulong bb, int ranks) => ranks > 0 ? bb >> (ranks << 3) : bb << -(ranks << 3);
+    }
+}
--- BitboardExplorer/Program.cs
+++ BitboardExplorer/Program.cs
@@ -1,6 +1,7 @@
 ﻿using MinimalChess;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Globalization;
 using System.Runtime.Intrinsics.X86;
 
@@ -49,90 +50,26 @@ namespace BitboardExplorer
                 {
                     bitboard = bitboard << int.Parse(tokens[1]);
                 }
+                else if(command == "bis" && tokens.Length== 2)
+                {
+                    if (!int.TryParse(tokens[1], out int square))
+                        square = Notation.ToSquare(tokens[1]);
+                    FindBishopTargetsAnnotated(bitboard, square);
+                    Console.WriteLine("Bitboard.GenBishop");
+                    PrintBitboard(Bitboard.GenBishop(bitboard, square));
+                }
                 else if(command == "rook" && tokens.Length == 2)
                 {
-                    int square;
-                    if(!int.TryParse(tokens[1], out square))
+                    if (!int.TryParse(tokens[1], out int square))
                         square = Notation.ToSquare(tokens[1]);
-                    ulong bbPiece = 1UL << square;
-                    Console.WriteLine("Piece:");
-                    PrintBitboard(bbPiece);
- 
-                    ulong bbBlocker = bitboard & ~bbPiece; /* remove the source square from the occupation */
-                    Console.WriteLine("Blocker:");
-                    PrintBitboard(bbBlocker);
-
-                    //mask the bits below bbPiece
-                    ulong bbBelow = bbPiece - 1;
-                    Console.WriteLine("Low bits:");
-                    PrintBitboard(bbBelow);
-
-                    //************************
-                    //** HORIZONTAL TARGETS **
-                    //************************
-
-                    //horizontal full line
-                    int file = square % 8;
-                    ulong bbHorizontal = 0x00000000000000FFUL << (square - file);
-                    Console.WriteLine("Horizontal:");
-                    PrintBitboard(bbHorizontal);
-
-                    ulong bbBlockersAbove = (bbBlocker & bbHorizontal) & ~bbBelow;
-                    Console.WriteLine("bbBlockersAbove");
-                    PrintBitboard(bbBlockersAbove);
-                    //x^(x-1) sets all bits up to and including the first set bit, the rest are zeroed out.
-                    ulong bbMaskAbove = bbBlockersAbove ^ (bbBlockersAbove - 1);
-                    //mask above has now all bits up to the first blocker above origin set
-                    Console.WriteLine("bbMaskAbove");
-                    PrintBitboard(bbMaskAbove);
-
-                    //count the bits until the first blocker below origin
-                    ulong bbBlockersBelow = (bbBlocker & bbHorizontal) & bbBelow;
-                    int lzcnt = (int)Lzcnt.X64.LeadingZeroCount(bbBlockersBelow | 1);
-                    //and shift a mask from the top to clear all bits up to that blocker
-                    ulong bbMaskBelow = 0x7FFFFFFFFFFFFFFFUL >> lzcnt;
-                    Console.WriteLine("bbMaskBelow");
-                    PrintBitboard(bbMaskBelow);
-
-                    //the difference between the two masks is our horizontal attack line (including the first occupied square)
-                    Console.WriteLine("hLine == (bbMaskAbove ^ bbMaskBelow) & bbHorizontal");
-                    ulong bbHorizontalAttacks = (bbMaskAbove ^ bbMaskBelow) & bbHorizontal;
-                    PrintBitboard(bbHorizontalAttacks);
-
-                    //**********************
-                    //** VERTICAL TARGETS **
-                    //**********************
-
-                    //vertical full line
-                    ulong bbVertical = 0x0101010101010101UL << file;
-                    Console.WriteLine("Vertical:");
-                    PrintBitboard(bbVertical);
-
-                    bbBlockersAbove = (bbBlocker & bbVertical) & ~bbBelow;
-                    Console.WriteLine("bbBlockersAbove");
-                    PrintBitboard(bbBlockersAbove);
-                    //x^(x-1) sets all bits up to and including the first set bit, the rest are zeroed out.
-                    bbMaskAbove = bbBlockersAbove ^ (bbBlockersAbove - 1);
-                    //mask above has now all bits up to the first blocker above origin set
-                    Console.WriteLine("bbMaskAbove");
-                    PrintBitboard(bbMaskAbove);
-
-                    //count the bits until the first blocker below origin
-                    bbBlockersBelow = (bbBlocker & bbVertical) & bbBelow;
-                    lzcnt = (int)Lzcnt.X64.LeadingZeroCount(bbBlockersBelow | 1);
-                    //and shift a mask from the top to clear all bits up to that blocker
-                    bbMaskBelow = 0x7FFFFFFFFFFFFFFFUL >> lzcnt;
-                    Console.WriteLine("bbMaskBelow");
-                    PrintBitboard(bbMaskBelow);
-
-                    //the difference between the two masks is our horizontal attack line (including the first occupied square)
-                    Console.WriteLine("vLine == (bbMaskAbove ^ bbMaskBelow) & bbVertical");
-                    ulong bbVerticalAttacks = (bbMaskAbove ^ bbMaskBelow) & bbVertical;
-                    PrintBitboard(bbMaskAbove ^ bbMaskBelow);
-
-                    Console.WriteLine("ROOK ATTACKS:");
-                    PrintBitboard(bbVerticalAttacks | bbHorizontalAttacks);
-                    Console.WriteLine("*****");
+                    FindRookTargetsAnnotated(bitboard, square);
+                    Console.WriteLine("Bitboard.GenRook");
+                    PrintBitboard(Bitboard.GenRook(bitboard, square));
+                }
+                else if(command == "Test")
+                {
+                    for (int i = 0; i < 64; i++)
+                        FindBishopTargetsAnnotated(bitboard, i);
                 }
                 else
                 {
@@ -152,6 +89,148 @@ namespace BitboardExplorer
                     }
                 }
             }
+        }
+
+        private static void FindBishopTargetsAnnotated(ulong bitboard, int square)
+        {
+            ulong bbPiece = 1UL << square;
+            Console.WriteLine("Piece:");
+            PrintBitboard(bbPiece);
+
+            //ulong bbBlocker = bitboard & ~bbPiece; /* remove the source square from the occupation */
+            //Console.WriteLine("Blocker:");
+            //PrintBitboard(bbBlocker);
+            //
+            ////mask the bits below bbPiece
+            //ulong bbBelow = bbPiece - 1;
+            //Console.WriteLine("Low bits:");
+            //PrintBitboard(bbBelow);
+
+            //************************
+            //** DIAGONALS TARGETS **
+            //************************
+
+            //https://www.chessprogramming.org/On_an_empty_Board
+
+            int diag = 8 * (square & 7) - (square & 56);
+            int north = -diag & (diag >> 31);
+            int south = diag & (-diag >> 31);
+            ulong bbDiag = (0x8040201008040201UL >> south) << north;
+            Console.WriteLine($"Diagonal diag {diag} north {north} south {south}");
+            PrintBitboard(bbDiag);
+
+            //************************
+            //** ANTI-DIAGONAL TARGETS **
+            //************************
+
+            diag = 56 - 8 * (square & 7) - (square & 56);
+            north = -diag & (diag >> 31);
+            south = diag & (-diag >> 31);
+            ulong bbAntiDiag = (0x0102040810204080UL >> south) << north;
+            Console.WriteLine($"Anti-Diagonal diag {diag} north {north} south {south}");
+            PrintBitboard(bbAntiDiag);
+
+            //************************
+
+            int rank = square / 8;
+            int file = square & 7;
+
+            const ulong DIAGONAL = 0x8040201008040201UL;
+            int verticalShift = file - rank;
+            int bitShift = verticalShift << 3;//to shift one rank means to shift by 8 bits
+            ulong bbDiag2 = bitShift > 0 ? DIAGONAL >> bitShift : DIAGONAL << -bitShift;
+            Debug.Assert(bbDiag == bbDiag2);
+
+
+            const ulong ANTIDIAGONAL = 0x0102040810204080;
+            verticalShift = 7 - file - rank;
+            bitShift = verticalShift << 3;//to shift one rank means to shift by 8 bits
+            ulong bbAntiDiag2 = bitShift > 0 ? ANTIDIAGONAL >> bitShift : ANTIDIAGONAL << -bitShift;
+            Debug.Assert(bbAntiDiag == bbAntiDiag2);
+
+        }
+
+        private static void FindRookTargetsAnnotated(ulong bitboard, int square)
+        {
+            ulong bbPiece = 1UL << square;
+            Console.WriteLine("Piece:");
+            PrintBitboard(bbPiece);
+
+            ulong bbBlocker = bitboard & ~bbPiece; /* remove the source square from the occupation */
+            Console.WriteLine("Blocker:");
+            PrintBitboard(bbBlocker);
+
+            //mask the bits below bbPiece
+            ulong bbBelow = bbPiece - 1;
+            Console.WriteLine("Low bits:");
+            PrintBitboard(bbBelow);
+
+            //************************
+            //** HORIZONTAL TARGETS **
+            //************************
+
+            //horizontal full line
+            int file = square % 8;
+            ulong bbHorizontal = 0x00000000000000FFUL << (square - file);
+            Console.WriteLine("Horizontal:");
+            PrintBitboard(bbHorizontal);
+
+            ulong bbBlockersAbove = (bbBlocker & bbHorizontal) & ~bbBelow;
+            Console.WriteLine("bbBlockersAbove");
+            PrintBitboard(bbBlockersAbove);
+            //x^(x-1) sets all bits up to and including the first set bit, the rest are zeroed out.
+            ulong bbMaskAbove = bbBlockersAbove ^ (bbBlockersAbove - 1);
+            //mask above has now all bits up to the first blocker above origin set
+            Console.WriteLine("bbMaskAbove");
+            PrintBitboard(bbMaskAbove);
+
+            //count the bits until the first blocker below origin
+            ulong bbBlockersBelow = (bbBlocker & bbHorizontal) & bbBelow;
+            int lzcnt = (int)Lzcnt.X64.LeadingZeroCount(bbBlockersBelow | 1);
+            //and shift a mask from the top to clear all bits up to that blocker
+            ulong bbMaskBelow = 0x7FFFFFFFFFFFFFFFUL >> lzcnt;
+            Console.WriteLine("bbMaskBelow");
+            PrintBitboard(bbMaskBelow);
+
+            //the difference between the two masks is our horizontal attack line (including the first occupied square)
+            Console.WriteLine("hLine == (bbMaskAbove ^ bbMaskBelow) & bbHorizontal");
+            ulong bbHorizontalAttacks = (bbMaskAbove ^ bbMaskBelow) & bbHorizontal;
+            PrintBitboard(bbHorizontalAttacks);
+
+            //**********************
+            //** VERTICAL TARGETS **
+            //**********************
+
+            //vertical full line
+            ulong bbVertical = 0x0101010101010101UL << file;
+            Console.WriteLine("Vertical:");
+            PrintBitboard(bbVertical);
+
+            bbBlockersAbove = (bbBlocker & bbVertical) & ~bbBelow;
+            Console.WriteLine("bbBlockersAbove");
+            PrintBitboard(bbBlockersAbove);
+            //x^(x-1) sets all bits up to and including the first set bit, the rest are zeroed out.
+            bbMaskAbove = bbBlockersAbove ^ (bbBlockersAbove - 1);
+            //mask above has now all bits up to the first blocker above origin set
+            Console.WriteLine("bbMaskAbove");
+            PrintBitboard(bbMaskAbove);
+
+            //count the bits until the first blocker below origin
+            bbBlockersBelow = (bbBlocker & bbVertical) & bbBelow;
+            lzcnt = (int)Lzcnt.X64.LeadingZeroCount(bbBlockersBelow | 1);
+            //and shift a mask from the top to clear all bits up to that blocker
+            bbMaskBelow = 0x7FFFFFFFFFFFFFFFUL >> lzcnt;
+            Console.WriteLine("bbMaskBelow");
+            PrintBitboard(bbMaskBelow);
+
+            //the difference between the two masks is our horizontal attack line (including the first occupied square)
+            Console.WriteLine("vLine == (bbMaskAbove ^ bbMaskBelow) & bbVertical");
+            ulong bbVerticalAttacks = (bbMaskAbove ^ bbMaskBelow) & bbVertical;
+            PrintBitboard(bbMaskAbove ^ bbMaskBelow);
+
+            Console.WriteLine("ROOK ATTACKS:");
+            PrintBitboard(bbVerticalAttacks | bbHorizontalAttacks);
+            Console.WriteLine("*****");
         }
 
         private static void PrintHex(ulong bitboard)
